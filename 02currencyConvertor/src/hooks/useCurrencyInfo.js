3fb95a8db81039a40d52a27da5778e865454fdb5
Raju Chougale <rjchgl08@gmail.com>
2024-04-08 18:51:08 +0530

    Updated currency.md

---
+++ 02currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,9 @@
+import { useEffect } from 'react';
+
+function useCurrencyInfo(currency) {
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    );
+  }, []);
+}
