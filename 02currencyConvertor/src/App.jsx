cc6490295292d4ffef6a8ef45422dbdaf6084ee9
Raju Chougale <rjchgl08@gmail.com>
2024-04-09 10:02:10 +0530

    code updated

--- 02currencyConvertor/src/App.jsx
+++ 02currencyConvertor/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { InputBox } from './components';
 import useCurrencyInfo from './hooks/useCurrencyInfo';
-import './App.css';
 
 function App() {
   const [amount, setAmount] = useState();
