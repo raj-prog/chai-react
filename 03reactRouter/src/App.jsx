2d36a3688d12da3bb55cfe92c69ae7cfeef94c0e
Raju Chougale <rjchgl08@gmail.com>
2024-04-09 18:25:27 +0530

    header-footer component created

--- 03reactRouter/src/App.jsx
+++ 03reactRouter/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
@@ -8,7 +6,9 @@ function App() {
 
   return (
     <>
-      <h1 className='text-3xl text-center'>React router</h1>
+      <h1 className='text-3xl text-center bg-green-600 p-4 text-white'>
+        React router
+      </h1>
     </>
   );
 }
