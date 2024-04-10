b1cd8cb688199dbfb905397a8b7e783890b019ad
Raju Chougale <rjchgl08@gmail.com>
2024-04-10 09:44:37 +0530

    about component & routes added

--- 03reactRouter/src/App.jsx
+++ 03reactRouter/src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1 className='text-3xl text-center bg-green-600 p-4 text-white'>
-        React router
-      </h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
