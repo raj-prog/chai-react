d3a60ec528063df5b68d0a47c9a2d81aa34d9a4e
Raju Chougale <rjchgl08@gmail.com>
2024-04-10 18:05:59 +0530

    github component created

---
+++ 03reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Github() {
+  return (
+    <div className='text-center m-5 bg-gray-600 text-white p-4 text-3xl'>
+      Github followers:
+    </div>
+  );
+}
+
+export default Github;
--- 03reactRouter/src/components/User/User.jsx
+++ 03reactRouter/src/components/User/User.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 const User = () => {
   const { userid } = useParams();
   return (
-    <div className='bg-green-500 text-white p-4 text-2xl'>User: {userid}</div>
+    <div className='bg-gray-600 text-white p-4 text-2xl'>User: {userid}</div>
   );
 };
 
