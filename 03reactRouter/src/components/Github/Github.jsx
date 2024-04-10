b40464a048159366d599749ed8fb4d5b2242edc5
Raju Chougale <rjchgl08@gmail.com>
2024-04-10 18:54:16 +0530

    fetch api used in github component

--- 03reactRouter/src/components/Github/Github.jsx
+++ 03reactRouter/src/components/Github/Github.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 function Github() {
+  const data = useLoaderData();
+
+  //   const [data, setData] = useState([]);
+  //   useEffect(() => {
+  //     fetch('https://api.github.com/users/hiteshchoudhary')
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         console.log(data);
+  //         setData(data);
+  //       });
+  //   }, []);
+
   return (
     <div className='text-center m-5 bg-gray-600 text-white p-4 text-3xl'>
-      Github followers:
+      Github followers: {data.followers}
+      <div>
+        <img
+          className='inline-block mx-auto my-4'
+          src={data.avatar_url}
+          alt='Git picture'
+          width={300}
+        />
+      </div>
     </div>
   );
 }
 
 export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch('https://api.github.com/users/hiteshchoudhary');
+  return response.json();
+};
