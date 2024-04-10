2315f79137cfe227fa79f5ae556e9f44ec3e3f0e
Raju Chougale <rjchgl08@gmail.com>
2024-04-10 14:29:41 +0530

    useParams hook created

---
+++ 03reactRouter/src/components/User/User.jsx
@@ -0,0 +1,10 @@
+import { useParams } from 'react-router-dom';
+
+const User = () => {
+  const { userid } = useParams();
+  return (
+    <div className='bg-green-500 text-white p-4 text-2xl'>User: {userid}</div>
+  );
+};
+
+export default User;
