0dbab3d49ac5eab8c913894f05c640e3bf897e0e
Raju Chougale <rjchgl08@gmail.com>
2024-04-14 18:55:42 +0530

    useState hook implemented

--- 09themeSwitcher/src/App.jsx
+++ 09themeSwitcher/src/App.jsx
@@ -1,16 +1,30 @@
+import { useState } from 'react';
 import './App.css';
+import { ThemeProvider } from './contexts/theme';
 
 function App() {
-  return (
-    <div className='flex flex-wrap min-h-screen items-center'>
-      <div className='w-full'>
-        <div className='w-full max-w-sm mx-auto flex justify-end mb-4'>
-          {/* themeBtn */}
-        </div>
+  const [themeMode, setThemeMode] = useState('light');
 
-        <div className='w-full max-w-sm mx-auto'>{/* Card */}</div>
+  const lightTheme = () => {
+    setThemeMode('light');
+  };
+
+  const darkTheme = () => {
+    setThemeMode('dark');
+  };
+
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className='flex flex-wrap min-h-screen items-center'>
+        <div className='w-full'>
+          <div className='w-full max-w-sm mx-auto flex justify-end mb-4'>
+            {/* themeBtn */}
+          </div>
+
+          <div className='w-full max-w-sm mx-auto'>{/* Card */}</div>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
