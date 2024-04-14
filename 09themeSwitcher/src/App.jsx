92d836a35a08b36215a91e04877f541c9587b1f3
Raju Chougale <rjchgl08@gmail.com>
2024-04-14 18:58:49 +0530

    useEffect hook created

--- 09themeSwitcher/src/App.jsx
+++ 09themeSwitcher/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { ThemeProvider } from './contexts/theme';
 
@@ -12,6 +12,12 @@ function App() {
   const darkTheme = () => {
     setThemeMode('dark');
   };
+
+  // actual change in theme
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light', 'dark');
+    document.querySelector('html').classList.add(themeMode);
+  }, [themeMode]);
 
   return (
     <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
